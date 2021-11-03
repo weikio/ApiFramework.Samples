078e2202768f890d8bdfadd376c0e4655bc6ecdc
Miikka Valkonen <miikka.valkonen@adafy.com>
2021-11-03 11:08:44 +0200

    Updated quickstart to ApiFramework version 1.1.0

--- quickstart/Startup.cs
+++ quickstart/Startup.cs
@@ -1,15 +1,11 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Weikio.ApiFramework.Abstractions;
-using Weikio.ApiFramework.ApiProviders.PluginFramework;
-using Weikio.ApiFramework.AspNetCore;
-using Weikio.ApiFramework.AspNetCore.StarterKit;
-using Weikio.ApiFramework.Core.Extensions;
+using System;
+using Weikio.ApiFramework;
 using Weikio.ApiFramework.Plugins.OpenApi;
 using Weikio.ApiFramework.Plugins.SqlServer;
 using Weikio.ApiFramework.Plugins.SqlServer.Configuration;
@@ -28,43 +24,47 @@ namespace quickstart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             Func<ILogger<Startup>, string, string> helloApi = (logger, name) =>
             {
                 logger.LogInformation("Running the API using parameter {Name}", name);
                 return $"Hello {name}!";
             };
-            
-            services.AddControllers();
 
             services.AddApiFrameworkStarterKit()
                 .AddApi(helloApi)
                 .AddApi<CalculatorApi>()
-                .AddSqlServer()
-                .AddOpenApi()
+                .AddSqlServer("/sqlserver",
+                    new SqlServerOptions
+                    {
+                        ConnectionString =
+                            "Server=tcp:adafydevtestdb001.database.windows.net,1433;User ID=docs;Password=3h1@*6PXrldU4F95;Integrated Security=false;Initial Catalog=adafyweikiodevtestdb001;"
+                    })
+                .AddSqlServer("/sqlserver_products",
+                    new SqlServerOptions
+                    {
+                        ConnectionString =
+                            "Server=tcp:adafydevtestdb001.database.windows.net,1433;User ID=docs;Password=3h1@*6PXrldU4F95;Integrated Security=false;Initial Catalog=adafyweikiodevtestdb001;",
+                        Tables = new[] {"Product*"},
+                    })
+                .AddOpenApi("/pets",
+                    new ApiOptions
+                    {
+                        SpecificationUrl = "https://petstore.swagger.io/v2/swagger.json", 
+                        IncludeHttpMethods = new[] { "GET" }
+                    }
+                )
+                .AddOpenApi("/pets_writeonly",
+                    new ApiOptions
+                    {
+                        SpecificationUrl = "https://petstore.swagger.io/v2/swagger.json", 
+                        ExcludeHttpMethods = new[] { "GET" }
+                    }
+                )
                 .AddEndpoint<CalculatorApi>("/mycal", 15)
                 .AddEndpoint<CalculatorApi>("/second", 100)
-                .AddEndpoint<CalculatorApi>("/third", 0)
-                .AddEndpoint("/sqlserver", ("Weikio.ApiFramework.Plugins.SqlServer", Version.Parse("1.1.0.0")), new SqlServerOptions()
-                {
-                    ConnectionString =
-                        "Server=tcp:adafydevtestdb001.database.windows.net,1433;User ID=docs;Password=3h1@*6PXrldU4F95;Integrated Security=false;Initial Catalog=adafyweikiodevtestdb001;"
-                })
-                .AddEndpoint("/sqlserver_products",  ("Weikio.ApiFramework.Plugins.SqlServer", Version.Parse("1.1.0.0")), new SqlServerOptions()
-                {
-                    ConnectionString =
-                        "Server=tcp:adafydevtestdb001.database.windows.net,1433;User ID=docs;Password=3h1@*6PXrldU4F95;Integrated Security=false;Initial Catalog=adafyweikiodevtestdb001;",
-                    Tables = new[] { "Product*" },
-                })
-                .AddEndpoint("/pets", "Weikio.ApiFramework.Plugins.OpenApi",
-                    new ApiOptions() { SpecificationUrl = "https://petstore.swagger.io/v2/swagger.json", IncludeHttpMethods = new[] { "GET" } });
-            
-            var endpointDefinition = new EndpointDefinition("/pets_writeonly", "Weikio.ApiFramework.Plugins.OpenApi", 
-                new ApiOptions()
-                {
-                    SpecificationUrl = "https://petstore.swagger.io/v2/swagger.json", ExcludeHttpMethods = new[] { "GET" }
-                });
-
-            services.AddSingleton(endpointDefinition);
+                .AddEndpoint<CalculatorApi>("/third", 0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -87,7 +87,7 @@ namespace quickstart
             });
         }
     }
-    
+
     public class CalculatorApi
     {
         public int Configuration { get; set; }
